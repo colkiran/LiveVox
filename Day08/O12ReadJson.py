f12640d008ac6b3e71f01a4198ecd9dae3496e31
Kiran <colkiran@gmail.com>
2024-10-03 17:45:50 +0530

    commit

--- Day08/O12ReadJson.py
+++ Day08/O12ReadJson.py
@@ -13,7 +13,19 @@ for book in jsonFR['books']:
         print(k, "=>", v)
     print("-" * 60)
 
+print("-" * 60)
+# data is enclosed in single quotes
+empdata = '{"name": "Micheal", "age": 35, "desig": "MGR", "dept": "finance"}'
+print(f"empdata : {empdata}")
+print(type(empdata))
 
+# loads - json string document into python dictionary
+print("-" * 60)
+res = json.loads(empdata)
+print(f"res :{res}")
+print(type(res))
 
+for k, v in res.items():
+    print(k, "=>", v)
 
 
